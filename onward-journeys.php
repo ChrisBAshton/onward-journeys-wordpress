5513a3538139b02b61ccfead1f2d71a3ca8006af
ChrisBAshton <chrisashtonweb@gmail.com>
2018-05-08 16:40:19 +0100

    apply styling to onward journeys

--- onward-journeys.php
+++ onward-journeys.php
@@ -15,7 +15,13 @@ function create_onward_journeys($the_content) {
     return $oj->process($the_content);
 }
 
+function apply_onward_journey_styling() {
+    wp_register_style('onward_journeys', plugin_dir_url(__FILE__) . 'onward-journeys.css');
+    wp_enqueue_style('onward_journeys' );
+}
+
 add_filter('the_content', 'create_onward_journeys', 1);
+add_action('wp_enqueue_scripts', 'apply_onward_journey_styling');
 
 // stop WordPress adding <br /> and <p> tags inside my shortcode (credit: http://sww.co.nz/solution-to-wordpress-adding-br-and-p-tags-around-shortcodes/)
 remove_filter('the_content', 'wpautop');
